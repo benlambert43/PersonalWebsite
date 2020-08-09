a0069f7bf3119485cc78886aa55667a4a4012a20
benlambert43 <benlambert43@gmail.com>
2020-08-08 23:35:22 -0600

    changing background image

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,72 +9,70 @@ import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
-import MenuIcon from "@material-ui/icons/Menu";
-import NotificationsIcon from "@material-ui/icons/Notifications";
-import { mainListItems } from "./NavItems";
+import ListItem from "@material-ui/core/ListItem";
+import HomeIcon from "@material-ui/icons/Home";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import WebOutlinedIcon from "@material-ui/icons/WebOutlined";
+import SentimentVerySatisfiedOutlinedIcon from "@material-ui/icons/SentimentVerySatisfiedOutlined";
+import PhotoLibraryOutlinedIcon from "@material-ui/icons/PhotoLibraryOutlined";
+import background from "../assets/background.JPG";
+import "./NavStyling.css";
 
-const drawerWidth = 60;
+const mainListItems = (
+  <div>
+    <ListItem button>
+      <HomeIcon />
+    </ListItem>
+    <Divider />
+    <ListItem button>
+      <InfoOutlinedIcon />
+    </ListItem>
+    <Divider />
+    <ListItem button>
+      <WebOutlinedIcon />
+    </ListItem>
+    <Divider />
+
+    <ListItem button>
+      <PhotoLibraryOutlinedIcon />
+    </ListItem>
+    <Divider />
+
+    <ListItem button>
+      <SentimentVerySatisfiedOutlinedIcon />
+    </ListItem>
+    <Divider />
+  </div>
+);
+
+const drawerWidth = 55;
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
   },
 
-  toolbarIcon: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-end",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-  },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+    maxHeight: 55,
+    minHeight: 55,
+    backgroundColor: "#546F93",
+    color: "#FFFFFF",
   },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: "none",
-  },
+  appBarShift: {},
+
   title: {
     flexGrow: 1,
   },
   drawerPaper: {
+    marginTop: 55,
     position: "relative",
-    whiteSpace: "nowrap",
     width: drawerWidth,
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+    height: `calc(100% - 55px)`,
   },
-  drawerPaperClose: {
-    overflowX: "hidden",
-    transition: theme.transitions.create("width", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up("sm")]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
+
+  appBarSpacer: { marginTop: 55 },
   content: {
     flexGrow: 1,
     height: "100vh",
@@ -83,48 +81,43 @@ const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(4),
     paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: "flex",
-    overflow: "auto",
-    flexDirection: "column",
-  },
-  fixedHeight: {
-    height: 240,
+
+    /* Responsive Full Background Image Using CSS
+     * Tutorial URL: http://sixrevisions.com/css/responsive-background-image/
+     */
+    body: {
+      /* Location of the image */
+      backgroundImage: `${background}`,
+
+      /* Image is centered vertically and horizontally at all times */
+      backgroundPosition: " center center",
+
+      /* Image doesn't repeat */
+      backgroundRepeat: "no-repeat",
+
+      /* Makes the image fixed in the viewport so that it doesn't move when 
+     the content height is greater than the image height */
+      backgroundAttachment: "fixed",
+
+      /* This is what makes the background image rescale based on its container's size */
+      backgroundSize: "cover",
+
+      /* Pick a solid background color that will be displayed while the background image is loading */
+      backgroundColor: "#464646",
+    },
+
+    minHeight: `calc(100% - 61px)`,
   },
 }));
 
 export default function Navigation() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar
-        position="absolute"
-        className={clsx(classes.appBar, open && classes.appBarShift)}
-      >
+      <AppBar position="absolute" className={clsx(classes.appBar)}>
         <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(
-              classes.menuButton,
-              open && classes.menuButtonHidden
-            )}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             component="h1"
             variant="h6"
@@ -132,29 +125,26 @@ export default function Navigation() {
             noWrap
             className={classes.title}
           >
-            Dashboard
+            Ben Lambert
           </Typography>
-          <IconButton color="inherit">
-            <Badge badgeContent={4} color="secondary">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
+          <IconButton color="inherit"></IconButton>
         </Toolbar>
       </AppBar>
       <Drawer
         variant="permanent"
         classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          paper: clsx(classes.drawerPaper),
         }}
         open={true}
       >
-        <div className={classes.toolbarIcon}></div>
         <List>{mainListItems}</List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <Paper style={{ minHeight: 900 }} />
+          <Typography align={"right"} style={{ color: "white", fontSize: 10 }}>
+            Dent d'Arclusaz, France
+          </Typography>
         </Container>
       </main>
     </div>
